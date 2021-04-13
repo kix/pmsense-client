83c6bd97b08875702eac2391c2e2083004c03745
Stepan Anchugov <kixxx1@gmail.com>
2021-04-13 15:26:10 +0500

    Updates

---
+++ precache-manifest.8907a51a334ba6ca3541b475ce3ffd02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c279e064611e5b5932a576f1267c4d",
+    "url": "/pmsense-client/index.html"
+  },
+  {
+    "revision": "2cdcf3738f9fa4cbe524",
+    "url": "/pmsense-client/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c192fba48a29692d2a6b",
+    "url": "/pmsense-client/static/js/2.0ad90753.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pmsense-client/static/js/2.0ad90753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cdcf3738f9fa4cbe524",
+    "url": "/pmsense-client/static/js/main.e1ca1a01.chunk.js"
+  },
+  {
+    "revision": "f1e040b429f27d93135c",
+    "url": "/pmsense-client/static/js/runtime-main.58caffb2.js"
+  }
+]);
